35721aefcb9e62462b31de7ad0629fa1b74b6990
IamGroot <107915199+sharon111edapal@users.noreply.github.com>
2024-01-22 12:03:58 +0400

    Update RakApplication.java

--- src/main/java/com/example/rak/RakApplication.java
+++ src/main/java/com/example/rak/RakApplication.java
@@ -2,33 +2,6 @@ package com.example.rak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@SpringBootApplication
-public class RakApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(RakApplication.class, args);
-    }
-
-}
-
-@RestController
-@RequestMapping("/api")
-class MyController {
-
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, Spring Boot!";
-    }
-}
-
-/* package com.example.rak;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RakApplication {
@@ -38,4 +11,3 @@ public class RakApplication {
 	}
 
 }
-*/
